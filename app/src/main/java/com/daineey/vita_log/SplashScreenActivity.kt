709c263b3381fb76ff371db6e261f8ed05fdf9ea
daineey <dain0403@naver.com>
2023-06-13 03:44:28 +0900

    update 0.16

---
+++ app/src/main/java/com/daineey/vita_log/SplashScreenActivity.kt
@@ -0,0 +1,21 @@
+package com.daineey.vita_log
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)  // Delay for 2 seconds (3000ms)
+    }
+}
