8e3926c7e477135fe47f8fab13c4b901c6df6253
Yeongjin Kim <kyj950514@naver.com>
2023-06-21 14:12:21 +0900

    Update 0.15

--- app/src/main/java/com/daineey/vita_log/MainActivity.kt
+++ app/src/main/java/com/daineey/vita_log/MainActivity.kt
@@ -9,10 +9,8 @@ import com.daineey.vita_log.ui.home.HomeFragment
 
 
 private const val TAG_HOME = "home_fragment"
-private const val TAG_PROFILE = "profile_fragment"
 private const val TAG_SEARCH = "search_fragment"
 private const val TAG_CHAT = "chat_fragment"
-private const val TAG_MY = "my_fragment"
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,10 +26,8 @@ class MainActivity : AppCompatActivity() {
         binding.navigationView.setOnItemSelectedListener { item ->
             when(item.itemId) {
                 R.id.home -> setFragment(TAG_HOME, com.daineey.vita_log.ui.home.HomeFragment())
-                R.id.profile -> setFragment(TAG_PROFILE, com.daineey.vita_log.ui.profile.ProfileFragment())
                 R.id.search_photo-> setFragment(TAG_SEARCH, com.daineey.vita_log.ui.search.SearchFragment())
                 R.id.chat-> setFragment(TAG_CHAT, com.daineey.vita_log.ui.chat.ChatFragment())
-                R.id.my-> setFragment(TAG_MY, com.daineey.vita_log.ui.my.MyFragment())
             }
             true
         }
@@ -51,10 +47,8 @@ class MainActivity : AppCompatActivity() {
         if (fragment == null){
             fragment = when (tag) {
                 TAG_HOME -> com.daineey.vita_log.ui.home.HomeFragment()
-                TAG_PROFILE -> com.daineey.vita_log.ui.profile.ProfileFragment()
                 TAG_SEARCH -> com.daineey.vita_log.ui.search.SearchFragment()
                 TAG_CHAT -> com.daineey.vita_log.ui.chat.ChatFragment()
-                TAG_MY -> com.daineey.vita_log.ui.my.MyFragment()
                 else -> throw IllegalArgumentException("Unexpected tag: $tag")
             }
             fragTransaction.add(R.id.fragmentContainer, fragment, tag)
