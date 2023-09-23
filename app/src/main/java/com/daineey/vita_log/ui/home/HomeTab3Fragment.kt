f78bda139246bb71ffe163417967c8cef2fcf193
kyj950514 <kyj950514@naver.com>
2023-09-24 01:10:50 +0900

    feat:LLM AI Chatbot implementation

--- app/src/main/java/com/daineey/vita_log/ui/home/HomeTab3Fragment.kt
+++ app/src/main/java/com/daineey/vita_log/ui/home/HomeTab3Fragment.kt
@@ -77,6 +77,46 @@ class HomeTab3Fragment : Fragment() {
             var intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://n.news.naver.com/mnews/article/586/0000027716?sid=103"))
             startActivity(intent)
         }
+        val button8 = view?.findViewById<ImageView>(R.id.article8)
+        button8?.setOnClickListener {
+            var intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://n.news.naver.com/mnews/article/346/0000029747?sid=103"))
+            startActivity(intent)
+        }
+        val button9 = view?.findViewById<ImageView>(R.id.article9)
+        button9?.setOnClickListener {
+            var intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://n.news.naver.com/mnews/article/014/0004346095?sid=103"))
+            startActivity(intent)
+        }
+        val button10 = view?.findViewById<ImageView>(R.id.article10)
+        button10?.setOnClickListener {
+            var intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://n.news.naver.com/mnews/article/346/0000030609?sid=103"))
+            startActivity(intent)
+        }
+        val button11 = view?.findViewById<ImageView>(R.id.article11)
+        button11?.setOnClickListener {
+            var intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://n.news.naver.com/mnews/article/346/0000027710?sid=103"))
+            startActivity(intent)
+        }
+        val button12 = view?.findViewById<ImageView>(R.id.article12)
+        button12?.setOnClickListener {
+            var intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://n.news.naver.com/mnews/article/022/0003388677?sid=103"))
+            startActivity(intent)
+        }
+        val button13 = view?.findViewById<ImageView>(R.id.article13)
+        button13?.setOnClickListener {
+            var intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://n.news.naver.com/mnews/article/296/0000041918?sid=103"))
+            startActivity(intent)
+        }
+        val button14 = view?.findViewById<ImageView>(R.id.article14)
+        button14?.setOnClickListener {
+            var intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://n.news.naver.com/mnews/article/014/0004255696?sid=103"))
+            startActivity(intent)
+        }
+        val button15 = view?.findViewById<ImageView>(R.id.article15)
+        button15?.setOnClickListener {
+            var intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://n.news.naver.com/mnews/article/469/0000389195?sid=103"))
+            startActivity(intent)
+        }
     }
 
     override fun onDestroyView() {
