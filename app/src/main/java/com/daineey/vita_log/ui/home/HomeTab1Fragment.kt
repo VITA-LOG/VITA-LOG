28648a4927662bdb6e77aadbad14ecc59c50fdb0
kyj950514 <kyj950514@naver.com>
2023-10-01 15:36:34 +0900

    fix: LLM AI Chatbot line feed

--- app/src/main/java/com/daineey/vita_log/ui/home/HomeTab1Fragment.kt
+++ app/src/main/java/com/daineey/vita_log/ui/home/HomeTab1Fragment.kt
@@ -1,16 +1,22 @@
 package com.daineey.vita_log.ui.home
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.daineey.vita_log.R
+import com.daineey.vita_log.SupplementActivity
 import com.daineey.vita_log.databinding.FragmentHometab1Binding
+import com.daineey.vita_log.my.QuestionActivity
 
 class HomeTab1Fragment : Fragment() {
 
@@ -37,6 +43,63 @@ class HomeTab1Fragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?){
         super.onViewCreated(view, savedInstanceState)
+
+        val button1 = view?.findViewById<ImageButton>(R.id.gut_health)
+        button1?.setOnClickListener {
+            activity?.let {
+                val intent = Intent(context, SupplementActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        val button2 = view?.findViewById<ImageButton>(R.id.junlipsun)
+        button2?.setOnClickListener {
+            activity?.let {
+                val intent = Intent(context, SupplementActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        val button3 = view?.findViewById<ImageButton>(R.id.daily_energy)
+        button3?.setOnClickListener {
+            activity?.let {
+                val intent = Intent(context, SupplementActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        val button4 = view?.findViewById<ImageButton>(R.id.sleep)
+        button4?.setOnClickListener {
+            activity?.let {
+                val intent = Intent(context, SupplementActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        val button5 = view?.findViewById<ImageButton>(R.id.exercise_supplement)
+        button5?.setOnClickListener {
+            activity?.let {
+                val intent = Intent(context, SupplementActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        val button6 = view?.findViewById<ImageButton>(R.id.skin_care)
+        button6?.setOnClickListener {
+            activity?.let {
+                val intent = Intent(context, SupplementActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        val button7 = view?.findViewById<ImageButton>(R.id.eyes)
+        button7?.setOnClickListener {
+            activity?.let {
+                val intent = Intent(context, SupplementActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        val button8 = view?.findViewById<ImageButton>(R.id.liver)
+        button8?.setOnClickListener {
+            activity?.let {
+                val intent = Intent(context, SupplementActivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
 
     override fun onDestroyView() {
